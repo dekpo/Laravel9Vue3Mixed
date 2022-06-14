8a618b01b6d927df5d9396c18f4c59c097faa073
Dekpo <dekpo@me.com>
2022-06-14 15:35:52 +0200

    Created RegisterController and register method

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\BaseController;
+use App\Models\User;
+use  Illuminate\Support\Facades\Auth;
+use  Illuminate\Support\Facades\Validator;
+
+class RegisterController extends BaseController
+{
+    /**
+     * Register api
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request){
+        // validate data
+        $validator = Validator::make($request->all(), [
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required',
+            'c_password'=>'required|same:password'
+        ]);
+        if ($validator->fails()){
+            return $this->sendError('Validation Error.',$validator->errors());
+        }
+        // input data and send token
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('MySecretKey')->plainTextToken;
+        $success['name'] = $user->name;
+        return $this->sendResponse($success, 'User  registered successfully !');
+    }
+}
