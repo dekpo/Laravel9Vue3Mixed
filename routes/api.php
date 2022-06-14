4c6dbfc22686f15e418cde246dd4500737326ff1
Dekpo <dekpo@me.com>
2022-06-14 14:55:11 +0200

    Added api Kernel and auth routes

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register','register');
+    Route::post('login','login');
+});
+
 Route::apiResource('picture','\App\Http\Controllers\PictureController');
