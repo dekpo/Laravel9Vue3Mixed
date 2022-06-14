11a4fb202a2dad43520805b5be5147b454d6de89
Dekpo <dekpo@me.com>
2022-06-14 12:19:19 +0200

    Created picture controller php artisan make:controller PictureController --api --model=Picture and added api route to the controler

---
+++ app/Http/Controllers/PictureController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Picture;
+use Illuminate\Http\Request;
+
+class PictureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        Picture::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Picture  $picture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Picture $picture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Picture  $picture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Picture $picture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Picture  $picture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Picture $picture)
+    {
+        //
+    }
+}
